80d53ad5c261c624e2d509cbbf0c4d1bd4bd3a60
haoxiang01 <276063373@qq.com>
2023-12-06 01:50:23 +0000

    add fChannel

--- Part_A/Wrappers/fChannel.m
+++ Part_A/Wrappers/fChannel.m
@@ -1,5 +1,5 @@
-% NAME, GROUP (EE4/MSc), 2010, Imperial College.
-% DATE
+% Haoxiang Huang, CSP (MSc), 2023, Imperial College.
+% 05-Dec-2023
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Models the channel effects in the system
@@ -24,3 +24,38 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function [symbolsOut]=fChannel(paths,symbolsIn,delay,beta,DOA,SNR,array)
+%% Parameters
+num_Tx = size(symbolsIn,2); % number of Tx
+len_in = size(symbolsIn,1);  % length of the source symbols
+len_out = len_in + max(delay); %length of the received symbols
+
+%use defin in ACT-3 slides p42
+N = size(array,1); % number of antennas
+L = num_Tx;
+M = len_in;
+symbolsOut = zeros(n,len_out);
+
+paths_index = 1;
+
+for i = 1:M
+    for j = 1:paths(i)
+        m_t_min_tao = zeros(len_out,1);
+        m_t_min_tao(1+delay(paths_index):L+delay(paths_index)) = symbolsIn(:,i);
+        amv = spv(array,DOA(paths_index,:));
+        x_t = amv * m_t_min_tao.' * beta(i);
+        symbolsOut = symbolsOut + x_t;
+        paths_index = paths_index + 1;
+    end
+end
+
+%%Add AWGN
+% desired signal power
+Pd_dB = 10*log10(sum(abs((beta(1:paths(1)) .* symbolsIn(1,1))).^2)); 
+
+% noise power
+Pn_dB = Pd_dB - SNR; 
+Pn = 10.^(Pn_dB/10);
+noise = sqrt(Pn/2) * (randn(size(symbolsOut)) + 1i * randn(size(symbolsOut))); % AWGN noise
+symbolsOut = symbolsOut + noise;                                                    % Add noise
+symbolsOut = symbolsOut.';
+end
