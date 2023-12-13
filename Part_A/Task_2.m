c6a1ac2447bacac744806bfd7f3282a984a6a5c2
haoxiang01 <276063373@qq.com>
2023-12-13 15:28:22 +0000

    polish comments and improve code structure

--- Part_A/Task_2.m
+++ Part_A/Task_2.m
@@ -1,47 +1,52 @@
 %...............................................
-% Author: Haoxiang Huang, MSc CSP, IC. 
-% Date: 05-Dec-2023.
-% This is the Task2 design for the ACT CW PartA
+% + Author: Haoxiang Huang, MSc CSP, IC. 
+% + Date: 06-Dec-2023.
+% + This is the Task2 design for the ACT CW PartA
+% + Please execute this script in the directory: '..\PartA\'
+% + Please ensure to include the util package 'Wrappers'
+%   and 'Photos' folder
 %...............................................
+
 clc;
 clear all;
 close all;
+
+%add the environment path of util packages
 addpath('Wrappers')
 
 %% Initialization and Load transmitted images
+%image environment path 
 img1_path = 'Photos\photo1.png';
 img2_path = 'Photos\photo2.png';
 img3_path = 'Photos\photo3.png';
 
 disp('........Initization: 3 Co-channel Transmiter.........');
 disp('Load three Images for three Users');
-Image1 = imread(img1_path);
-Image2 = imread(img2_path);
-Image3 = imread(img3_path);
-figure;
-subplot(1,3,1);
-imshow(Image1);
-title('Original Desired Image (User1)');
-subplot(1,3,2);
-imshow(Image2);
-title('Original Interference Image (User2)')
-subplot(1,3,3);
-imshow(Image3);
-title('Original Interference Image (User3)')
-[x1,y1,~] = size(Image1);
-[x2,y2,~] = size(Image2);
-[x3,y3,~] = size(Image3);
+% set the maximum width and height limits for the image
+% here limitation is required by the official doc
+width_limit = 160;
+height_limit = 112;
 
-Q1 = x1*y1*3*8;
-Q2 = x2*y2*3*8;
-Q3 = x3*y3*3*8;
-P = max([Q1,Q2,Q3]);
+% read and display images
+[bits,widths,heights] = fShowImg(img1_path,img2_path,img3_path,width_limit,height_limit);
 
-bitstream_img1 = fImageSource(img1_path,P);
-bitstream_img2 = fImageSource(img2_path,P);
-bitstream_img3 = fImageSource(img3_path,P);
+% color channels per pixel
+zPixel = 3;
 
-%% DSSS-QPSK Modulation
+% bits per integer
+bitInt = 8;
+widthMax = max(widths);
+heightMax = max(heights);
+
+%the maximum number of bits required for the image
+bitsMax = widthMax * heightMax * zPixel * bitInt;
+
+%imgs to bitstream
+bitstream_img1 = fImageSource(img1_path,bitsMax);
+bitstream_img2 = fImageSource(img2_path,bitsMax);
+bitstream_img3 = fImageSource(img3_path,bitsMax);
+
+%% DS-QPSK Modulation
 disp('Start DS-QPSK Modulation');
 X = 8; %alphabetical order of the 1st letter H of my surname Huang.
 Y = 8; %alphabetical order of the 1st letter H of my formal firstname Haoxiang.
@@ -60,24 +65,25 @@ m = length(coeffs1) - 1;
 % maximum period of the shift register
 N_c = 2.^m - 1; 
 
-% generate all possible the Gold Sequences
-GoldSeq_buffer = [];
+%generate all possible the Gold Sequences
+GoldSeq_buffer = zeros(N_c,N_c);
 
 for index = 1:N_c
-    GoldSeq_buffer = [GoldSeq_buffer fGoldSeq(mSeq1,mSeq2,index)];
+    GoldSeq_buffer(:,index) = fGoldSeq(mSeq1,mSeq2,index);
 end
+
 GoldSeq_buffer = [GoldSeq_buffer mSeq1 mSeq2];
 GoldSeq_buffer_trans = 1-2.*GoldSeq_buffer;
 
-% calculate shift delay of the sencond m seqence
-delay_threshold = 1 + mod(X + Y,12);
+% calculate shift delay lowerbound required by the official doc
+delay_lowerbound = 1 + mod(X + Y,12);
 delay=0;
 
-% find Balanced Gold Sequence delay index with satisfied condition
+% find Balanced Gold Sequence delay index with satisfied conditions
 for index = 1:N_c+2
-    if sum(GoldSeq_buffer_trans(:,index)) == -1 && index>= delay_threshold
+    if sum(GoldSeq_buffer_trans(:,index)) == -1 && index>= delay_lowerbound
         delay = index;
-        break
+        break;
     end
 end
 
@@ -89,7 +95,7 @@ Balanced_GoldSeq3 = GoldSeq_buffer(:,delay+2);
 %calculate the angle for DS-QPSK modulation
 phi = (X+2*Y) * pi/180;
 
-%get the symbols of transmitted three user images
+%get the mapped symbols of transmitted three user images using DS-QPSK
 symbols_img1 = fDSQPSKModulator(bitstream_img1, Balanced_GoldSeq1, phi);
 symbols_img2 = fDSQPSKModulator(bitstream_img2, Balanced_GoldSeq2, phi);
 symbols_img3 = fDSQPSKModulator(bitstream_img3, Balanced_GoldSeq3, phi);
@@ -97,8 +103,8 @@ Tx_symbols = [symbols_img1,symbols_img2,symbols_img3];
 disp('...................................................');
 fprintf('\n');
 
-%% Task-1a (SNR = 0 dB)
-disp('..............Task-2 (SNR = 0 dB).................')
+%% Task-2a (SNR = 0 dB)
+disp('..............Task-2a (SNR = 0 dB).................');
 %  Channel Paramater
 delays = [mod(X+Y,4); 4 + mod(X+Y,5); 9 + mod(X+Y,6); 8; 13];
 betas = [.8 ; .4*exp(-1i*40*pi/180) ; .8*exp(1i*80*pi/180); 0.5 ; 0.2];
@@ -106,10 +112,12 @@ DOAs = [30 0; 45 0; 20 0; 80 0; 150 0;];
 paths = [3,1,1];
 array = [0,0,0];
 SNR = 0;
-disp('.............Task-2 Channel Parameters..........');
+disp('..............Task-2a Channel Parameters............');
 disp(['SNR = ',num2str(SNR)]);
-disp(['Delay = ',num2str(delays(1)),',',num2str(delays(2)),',',num2str(delays(3))]);
-disp(['Beta = ',num2str(betas(1)),',',num2str(betas(2)),',',num2str(betas(3))]);
+disp(['Delay = ',num2str(delays(1)),', ',num2str(delays(2)),', ',num2str(delays(3)), ...
+    ', ' , num2str(delays(4)),', ' , num2str(delays(5))]);
+disp(['Beta = ',num2str(betas(1)),', ',num2str(betas(2)),', ',num2str(betas(3)) ...
+       ,', ',num2str(betas(4)),', ',num2str(betas(5))]);
 disp('...................................................');
 disp('Transmit the images through this channel');
 Rx_symbols = fChannel(paths,Tx_symbols,delays,betas,DOAs,SNR,array);
@@ -118,21 +126,29 @@ Rx_symbols = fChannel(paths,Tx_symbols,delays,betas,DOAs,SNR,array);
 fprintf('\n');
 disp('..............Task-1b Rake Receiver................');
 disp('Start Channel Estimation');
+
+% Perform channel estimation 
 delay_estimate1 = fChannelEstimation(Rx_symbols,Balanced_GoldSeq1,paths(1));
-disp(['The estimated photo-1 transmittion delay is: ',num2str(delay_estimate1)])
+disp(['The estimated photo-1 transmition delay is: ',num2str(delay_estimate1)]);
 disp('Start DSSS-QPSK Demodulation');
+
+% Demodulate
 Rx_bitstreams = fDSQPSKDemodulator(Rx_symbols,Balanced_GoldSeq1,phi,delay_estimate1,betas(1));
+
+% Calculate the Bit Error Rate (BER)
 [~,BER_0db] = biterr(Rx_bitstreams, bitstream_img1);
+
+% Print and plot
 disp(['BER = ',num2str(BER_0db)]);
 figure();
-fImageSink(Rx_bitstreams, Q1,x1,y1);
-title({'Task2-Multipath';'Rake Receiver: Desired Received Photo1 '; ['SNR=',num2str(SNR),'dB , ','BER=',num2str(BER_0db)]});
+fImageSink(Rx_bitstreams, bits(1), widths(1),heights(1));
+title({'Task2-Multipath';'Rake Receiver: Desired Received Photo1 '; ['SNR=',num2str(SNR),' dB , ','BER=',num2str(BER_0db)]});
 disp('...................................................');
 fprintf('\n');
 
 
-%% Task-1a (SNR = 40 dB)
-disp('..............Task-2 (SNR = 40 dB).................')
+%% Task-2b (SNR = 40 dB)
+disp('..............Task-2b (SNR = 40 dB).................');
 %  Channel Paramater
 delays = [mod(X+Y,4); 4 + mod(X+Y,5); 9 + mod(X+Y,6); 8; 13];
 betas = [.8 ; .4*exp(-1i*40*pi/180) ; .8*exp(1i*80*pi/180); 0.5 ; 0.2];
@@ -140,10 +156,12 @@ DOAs = [30 0; 45 0; 20 0; 80 0; 150 0;];
 paths = [3,1,1];
 array = [0,0,0];
 SNR = 40;
-disp('.............Task-2 Channel Parameters..........');
+disp('..............Task-2b Channel Parameters............');
 disp(['SNR = ',num2str(SNR)]);
-disp(['Delay = ',num2str(delays(1)),',',num2str(delays(2)),',',num2str(delays(3))]);
-disp(['Beta = ',num2str(betas(1)),',',num2str(betas(2)),',',num2str(betas(3))]);
+disp(['Delay = ',num2str(delays(1)),', ',num2str(delays(2)),', ',num2str(delays(3)), ...
+    ', ' , num2str(delays(4)),', ' , num2str(delays(5))]);
+disp(['Beta = ',num2str(betas(1)),', ',num2str(betas(2)),', ',num2str(betas(3)) ...
+       ,', ',num2str(betas(4)),', ',num2str(betas(5))]);
 disp('...................................................');
 disp('Transmit the images through this channel');
 Rx_symbols = fChannel(paths,Tx_symbols,delays,betas,DOAs,SNR,array);
@@ -152,14 +170,23 @@ Rx_symbols = fChannel(paths,Tx_symbols,delays,betas,DOAs,SNR,array);
 fprintf('\n');
 disp('..............Task-1b Rake Receiver................');
 disp('Start Channel Estimation');
+
+% Perform channel estimation 
 delay_estimate1 = fChannelEstimation(Rx_symbols,Balanced_GoldSeq1,paths(1));
-disp(['The estimated photo-1 transmittion delay is: ',num2str(delay_estimate1)])
-disp('Start DS-QPSK Demodulation');
+disp(['The estimated photo-1 transmition delay is: ',num2str(delay_estimate1)]);
+disp('Start DSSS-QPSK Demodulation');
+
+% Demodulate
 Rx_bitstreams = fDSQPSKDemodulator(Rx_symbols,Balanced_GoldSeq1,phi,delay_estimate1,betas(1));
-[~,BER_40db] = biterr(Rx_bitstreams, bitstream_img1);
-disp(['BER = ',num2str(BER_40db)]);
+
+% Calculate the Bit Error Rate (BER)
+[~,BER_0db] = biterr(Rx_bitstreams, bitstream_img1);
+
+% Print and plot
+disp(['BER = ',num2str(BER_0db)]);
 figure();
-fImageSink(Rx_bitstreams, Q1,x1,y1);
-title({'Task2-Multipath';'Rake Receiver: Desired Received Photo1 '; ['SNR=',num2str(SNR),' dB , ','BER=',num2str(BER_40db)]});
+fImageSink(Rx_bitstreams, bits(1), widths(1),heights(1));
+title({'Task2-Multipath';'Rake Receiver: Desired Received Photo1 '; ['SNR=',num2str(SNR),' dB , ','BER=',num2str(BER_0db)]});
 disp('...................................................');
 fprintf('\n');
+
